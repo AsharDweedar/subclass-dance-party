2a2a47274f141746a3d41b2ee0f9c8172e9b2ecf
AsharDweedar <ashar.dweedar@gmail.com>
2017-08-27 11:04:20 +0300

    last changes before end of day

--- src/dancer.js
+++ src/dancer.js
@@ -1,36 +1,36 @@
-// Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
   //var dancer = Object.create(makeDancer.prototype);
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.stepTimer = timeBetweenSteps ;
-  this.setPosition(top, left);
-  // this.setter = this.setPosition(top, left);
-  // this.stepper = this.step(timeBetweenSteps);
+  this.top = top ;
+  this.left = left ;
+  //this.setPosition(top, left);
+  //this.setter = this.setPosition(top, left);
+  //this.stepper = this.step(timeBetweenSteps);
   //return dancer;
 };
+// makeDancer.prototype.stepTimer = 1000;
 
 makeDancer.prototype.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
     var that = this;
     setTimeout(function (){
       console.log('l')
       that.step()
-      }, this.stepTimer);
+      }, that.stepTimer);
   
   }
 
  makeDancer.prototype.setPosition = function(top, left) {
-  var that = this;
+  //var that = this;
   // console.log(that)
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
     var styleSettings = {
       top: top,
       left: left
     };
-    that.$node.css(styleSettings);
+    this.$node.css(styleSettings);
     return;
  };
+ /*makeDancer.prototype.stepper = makeDancer.prototype.step(this.stepTimer);
 
+ makeDancer.prototype.setter = makeDancer.prototype.setPosition(this.top, this.left);*/
--- src/init.js
+++ src/init.js
@@ -23,14 +23,15 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    /*
     dancer.step(1000);
-    dancer.setPosition(200,200)
+    dancer.setPosition(200,200)*/
   });
 });
 
